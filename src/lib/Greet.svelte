b6e7fed2ff57e14957a6d6c65d2cd5d3c2304d92
Evan Allen <evanallen@vt.edu>
2024-01-21 14:40:21 -0500

    Added tauri and sveltekit start

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { invoke } from "@tauri-apps/api/tauri";
+
+    let name = '';
+    let greetMsg = '';
+
+    async function greet() {
+        greetMsg = await invoke('greet', { name })
+    }
+</script>
+
+<div>
+    <input placeholder="Enter a name..." bind:value="{name}" id="greet-input">
+    <button on:click="{greet}">Greet</button>
+    <p>{greetMsg}</p>
+</div>
