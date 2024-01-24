b5bfaf38acd359615cd2ed37d7f55ade770b380a
Evan Allen <evanallen@vt.edu>
2024-01-24 13:02:11 -0500

    continued canvas work

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
     import { Splitpanes, Pane } from "svelte-splitpanes";
-    import { Stage, Layer, Image, Circle } from "svelte-konva";
-	import { onMount, tick } from "svelte";
     import { open } from "@tauri-apps/api/dialog";
 	import Button from "$lib/components/ui/button/button.svelte";
 	import { convertFileSrc } from "@tauri-apps/api/tauri";
-	import Konva from "konva";
 
     let selected: null | string = null;
     let image: HTMLImageElement | undefined;
+    let imageWidth = 0;
+    let imageHeight = 0;
+    let containerWidth = 0;
+    let containerHeight = 0;
+
     async function uploadClicked() {
         selected = await open({
             filters: [{
@@ -22,36 +24,71 @@
 
             const img = document.createElement("img");
             img.src = selected;
+            imageWidth = img.width;
+            imageHeight = img.height;
             img.onload = () => {
                 image = img;
             }
         }
     }
 
-    let pcbTransform = {
-        translation: {
-            x: 0,
-            y: 0
-        }, 
-        scale: 1
-    };
-    let pcbTransformStr = `scale(${pcbTransform.scale}) translate(${pcbTransform.translation.x} ${pcbTransform.translation.y})`
+    function getImageMousePos(event: MouseEvent) {
+        if (!image) return;
+
+        let target = event.currentTarget as HTMLElement;
+
+        let bounds = target.getBoundingClientRect();
+        let imageCoords = {x: event.clientX - bounds.left - imageLeft, y: event.clientY - bounds.top - imageTop};
+
+        return imageCoords;
+    }
+
 
     let scaleBy = 1.20;
     function wheelHandler(event: WheelEvent) {
         if (!event.ctrlKey) {
             return;
         }
+        if (!image) return;
 
         event.preventDefault();
+        let target = event.currentTarget as HTMLElement;
 
-        pcbTransform.scale *= scaleBy ** (event.deltaY / 78);
-        pcbTransformStr = `scale(${pcbTransform.scale}) translate(${pcbTransform.translation.x} ${pcbTransform.translation.y})`
-        console.log("transforming ");
-        console.log(pcbTransformStr);
+        let actualScaleBy = scaleBy ** (-event.deltaY / 78);
+        imageWidth *= actualScaleBy;
+        imageHeight *= actualScaleBy;
+
+        let imageCoords = getImageMousePos(event);
+
+        // new coords = old coords * scale
+        // new coords - old coords = old coords * scale - old coords
+        // diff = old coords * (scale - 1)
+        let diffX = imageCoords!.x * (actualScaleBy - 1);
+        let diffY = imageCoords!.y * (actualScaleBy - 1);
+        target.scrollBy(diffX, diffY)
     }
 
-    $: console.log(pcbTransform);
+    $: console.log({imageWidth, imageHeight});
+    
+    let imageLeft = 0;
+    let imageTop = 0;
+    // let imageTransform = "translate(0, 0)";
+
+    function fixImage() {
+        if (imageWidth < containerWidth) {
+            imageLeft = (containerWidth / 2) - (imageWidth / 2);
+        } else {
+            imageLeft = 0;
+        }
+
+        if (imageHeight < containerHeight) {
+            imageTop = (containerHeight / 2) - (imageHeight / 2);
+        } else {
+            imageTop = 0;
+        }
+    }
+
+    $: imageWidth, imageHeight, containerWidth, containerHeight, fixImage();
 
 </script>
 
@@ -64,27 +101,15 @@
                 </Button>
                 <p>{selected ? `${selected} selected` : 'none selected'}</p>
             </div>
-            <!-- <div 
-                id="canvasContainer" 
-                class="overflow-auto min-h-0 h-full" 
-                on:wheel={ (e) => { if (e.ctrlKey) e.preventDefault() }}>
-                <Stage 
-                    config={{ width: 5000, height: 4000, draggable: true }} 
-                    bind:handle={stage} 
-                    style="width: 100%, height: 100%"
-                    on:wheel={wheelHandler}>
-                    <Layer bind:handle={bottomLayer}>
-                    </Layer>
-                    <Layer>
-                        <Image config={{ image, perfectDrawEnabled: false }} />
-                    </Layer> 
-                </Stage>
-            </div> -->
 
-            <div id="canvasContainer" class="overflow-auto min-h-0 h-full no-flicker"
-                 on:wheel={wheelHandler}>
+
+            <div id="canvasContainer" class="relative overflow-auto min-h-0 h-full no-flicker"
+                 on:wheel={wheelHandler}
+                 bind:clientWidth={containerWidth}
+                 bind:clientHeight={containerHeight}>
                 {#if selected}
-                    <img src="{selected}" class="no-flicker" alt="pic" style:transform={ `scale(${pcbTransform.scale})` } />
+                    <img src="{selected}" class="no-flicker" alt="pic" style:width={imageWidth}px style:height={imageHeight}px
+                         style:left={imageLeft}px style:top={imageTop}px />
                 {/if}
             </div>
         </div>
@@ -95,17 +120,11 @@
 <style>
     .no-flicker {
         image-rendering: pixelated;
-       transform-style: preserve-3d; backface-visibility: hidden;  
-       /* perspective: 1000; */
-       /* transition:all 0.1s ease-in-out; */
-       /* filter: blur(0); */
-       /* transform: translate3d(0,0,0); */
-       -webkit-transform-style: preserve-3d; -webkit-backface-visibility: hidden;  
-       /* -webkit-perspective: 1000; */
-       /* -webkit-transition:all 0.1s ease-in-out; */
-       /* -webkit-filter: blur(0); */
-       /* -webkit-transform: translate3d(0,0,0); */
-       /* -moz-backface-visibility:    hidden; */
-        /* /* -ms-backface-visibility:     hidden; */
     }
+
+    img {
+        max-width: none;
+        position: absolute;
+    }
+
 </style>
